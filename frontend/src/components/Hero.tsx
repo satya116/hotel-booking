80ab8fc417ebb4c1cdcad54a4712a00f58d170b4
satya <satya@extendtech.net>
2024-01-22 22:53:15 +0530

    init

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <div className="bg-green-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
